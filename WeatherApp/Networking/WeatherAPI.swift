5ffc785c676729227a57334069fa906a8a667fba
huskar95 <huskar95@icloud.com>
2019-04-05 09:50:48 -0500

    add weather api file

---
+++ WeatherApp/Networking/WeatherAPI.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherAPI.swift
+//  WeatherApp
+//
+//  Created by Husein Kareem on 4/5/19.
+//  Copyright © 2019 Husein Kareem. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherAPI {
+    
+}
