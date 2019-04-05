5b91f3f389f16640cc885f0e1631b5ac709e8d8d
huskar95 <huskar95@icloud.com>
2019-04-05 18:33:13 -0500

    some of the logic for the weather API

--- WeatherApp/Networking/WeatherAPI.swift
+++ WeatherApp/Networking/WeatherAPI.swift
@@ -7,7 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+typealias CurrentWeatherCompletion = (_ weatherData: WeatherData, _ error: Error?) -> Void
 
 struct WeatherAPI {
     
+    //MARK: Properties
+    //TODO: Obfuscate api key
+    private static var APIKey = "f3223b8ce36ebff56eca29103515a962"
+    private static var BaseURLString = "http://api.openweathermap.org/data/2.5/&units=imperial"
+    private static var CurrentWeatherEndpoint = "/weather?q="
+    
+    
+    static func fetchCurrentWeatherForCity(_ cityName: String, completion: @escaping CurrentWeatherCompletion) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let cityURLString = WeatherAPI.CurrentWeatherEndpoint + cityName
+        let urlString = WeatherAPI.BaseURLString + cityURLString + "&appid=" + WeatherAPI.APIKey
+    }
 }
